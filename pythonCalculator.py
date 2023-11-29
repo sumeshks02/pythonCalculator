a56c4af7db6dac05cb7a8338817e8f1aee133bc7
sumeshks02 <sumeshks02@gmail.com>
2023-11-29 18:13:12 +0300

    frame add

--- pythonCalculator.py
+++ pythonCalculator.py
@@ -1,1 +1,10 @@
+import tkinter
 from tkinter import *
+
+window = Tk()
+window.title("Calculator")
+window.geometry("570x600+250+100")
+window.resizable(False,False)
+window.config(bg='light blue')
+
+window.mainloop()
